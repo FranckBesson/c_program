8eaf250f3ba0e4cc861d4f77b4cc9f37e27ab27b
Unknown <martin.marcadet@imerir.com>
2017-06-28 23:01:48 +0200

    all fonctionnel

--- main.c
+++ main.c
@@ -5,38 +5,43 @@
 #include <unistd.h>
 #include <string.h>
 
-char** split(char* chaine, const char* delim);
 void initChaine(char* chaine, int taille);
 
 int main() {
-    int fd;
-    char hour[6];
-    char current[13] = "            ";
-    char forecast[13] = "            ";
-    int cpt = 0;
-    char* TabResult[3];
-    char newChar[13] = "            ";
-    char data[150];
+
+//=============== Variables ================//
+
+    int fd;                                 // Serial Port File
+    int cpt = 0;                            // Character counter
+    char hour[6];                           // Current Hour buffer
+    char current[13] = "            ";      // Current Weather buffer
+    char forecast[13] = "            ";     // Forecast Weather buffer
+    char newChar[13] = "            ";      // Character receive buffer
+    char data[150];                         // Data Send buffer
+
+//=============== Curl init ================//
+
     CURL *curl;
     CURLcode res;
-    curl_global_init(CURL_GLOBAL_ALL);
-    struct curl_slist *list = NULL;
+    curl_global_init(CURL_GLOBAL_ALL);      // Init Curl global
+    struct curl_slist *list = NULL;         // Create list curl
 
 
-    printf("\n");
-    if ((fd = open("/dev/ttyACM0", O_RDWR)) == -1) {
+//=============== Program ==================//
+
+    if ((fd = open("/dev/ttyACM0", O_RDWR)) == -1) {    // Opening and check Serial Port File
         perror("open");
         exit(-1);
     }
 
-    while (1){
-        curl = curl_easy_init();
-        read(fd, &newChar[cpt], 1);
-        if (newChar[cpt] == '!'){
-            close(fd);
-            return 1;
+while (1){                                  // Loop while the time elapses
+        curl = curl_easy_init();            // Init Curl easy
+        read(fd, &newChar[cpt], 1);         // Read Serial Port
+        if (newChar[cpt] == '!'){           // Arduino sent '!' if user push button stop
+            close(fd);                      // Closing serial port file
+            return 1;                       // Stop C program
         }
-        if (newChar[cpt] == '@'){
+        if (newChar[cpt] == '@'){           // '@' is the first character of
             cpt = -1;
             for (int i = 0; i < 3; ++i) {
                 while (newChar[cpt] != ',' && newChar[cpt] != '#') {
@@ -45,12 +50,11 @@ int main() {
                 }
                 if (newChar[cpt] == ',' || newChar[cpt] == '#') {
                     newChar[cpt] = '\0';
-                    TabResult[i] = newChar;
                     cpt = -1;
                     switch (i){
                         case 0:
                             strncpy(hour, newChar, sizeof(newChar));
-                            printf("hour : %s\n", hour);
+                            printf("\nhour : %s\n", hour);
                             break;
                         case 1:
                             strncpy(current, newChar, sizeof(newChar));
@@ -85,48 +89,14 @@ int main() {
                 if (res != CURLE_OK)
                     fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
                 curl_easy_cleanup(curl);
+                curl_slist_free_all(list);
+                list = NULL;
             }
         }
     }
     close(fd);
     curl_global_cleanup();
     return 0;
-}
-
-char** split(char* chaine, const char* delim){
-    char** tab = NULL;
-    char* ptr;
-    int sizeStr;
-    int sizeTab = 0;
-    char* largestring;
-
-    int sizeDelim = strlen(delim);
-
-    largestring = chaine;
-
-    while ((ptr = strstr(largestring, delim)) != NULL){
-        sizeStr = ptr - largestring;
-        sizeTab++;
-        tab = (char**) realloc(tab, sizeof(char*)*sizeTab);
-        tab[sizeTab-1] = (char*) malloc(sizeof(char)*(sizeStr + 1));
-        strncpy(tab[sizeTab - 1], largestring, sizeStr);
-        tab[sizeTab - 1][sizeStr] = '\0';
-    }
-    ptr = ptr + sizeDelim;
-    largestring = ptr;
-    if ((strlen(largestring)) != 0){
-        sizeStr = strlen(largestring);
-        sizeTab ++;
-        tab= (char**) realloc(tab,sizeof(char*)*sizeTab);
-        tab[sizeTab-1]=(char*) malloc( sizeof(char)*(sizeStr+1) );
-        strncpy(tab[sizeTab-1],largestring,sizeStr);
-        tab[sizeTab-1][sizeStr]='\0';
-    }
-    sizeTab ++;
-    tab = (char**) realloc(tab, sizeof(char*)*sizeTab);
-    tab[sizeTab - 1] = NULL;
-
-    return tab;
 }
 
 void initChaine(char* chaine, int taille){
